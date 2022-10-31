ff5820007d96cc11e8214a135fa1a6e2ae0aed02
Marek Fadrny <marin@fadrny.com>
2022-10-31 22:11:51 +0100

    translations works

--- translate/test.py
+++ translate/test.py
@@ -1,12 +1,16 @@
 import deepl
 import codecs
+import glob
 
-auth_key = "391b96da-8804-493f-bf1b-8922d9d85040:fx"  # Replace with your key
+auth_key = ""  # Replace with your key
 translator = deepl.Translator(auth_key)
 
-fr=codecs.open("test.html", 'r')
-result = translator.translate_text(fr.read(), target_lang="SK", source_lang="CS", split_sentences="nonewlines", tag_handling="html")
-fr.close()
-f = open("output.html", "w")
-f.write(result.text)
-f.close()
+for f in glob.glob('../**/*.html', recursive=True):
+    fr=codecs.open(f, 'r')
+    result = translator.translate_text(fr.read(), target_lang="SK", source_lang="CS", split_sentences="nonewlines", tag_handling="html")
+    fr.close()
+    newFileName = f.replace(".html", ".sk.html")
+    f = open(newFileName, "w")
+    f.write(result.text)
+    f.close()
+    print("Succesfully created file: " + newFileName)
