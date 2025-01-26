73c99ef5b1aa17a532c1bcff675807e1a6dfc82e
fadrny <48125221+fadrny@users.noreply.github.com>
2025-01-26 14:58:59 +0100

    new sk translations

--- translate.py
+++ translate.py
@@ -6,11 +6,11 @@ auth_key = ""  # Replace with your key
 translator = deepl.Translator(auth_key)
 
 for f in glob.glob('**/*.html', recursive=True):
-    fr=codecs.open(f, 'r')
-    result = translator.translate_text(fr.read(), target_lang="SK", source_lang="CS", split_sentences="nonewlines", tag_handling="html")
-    fr.close()
+    with codecs.open(f, 'r', encoding='utf-8') as fr:
+        result = translator.translate_text(fr.read(), target_lang="SK", source_lang="CS", split_sentences="nonewlines", tag_handling="html")
+    
     newFileName = f.replace(".html", ".sk.html")
-    f = open(newFileName, "w")
-    f.write(result.text)
-    f.close()
-    print("Succesfully created file: " + newFileName)
+    with open(newFileName, "w", encoding='utf-8') as fw:
+        fw.write(result.text)
+    
+    print("Successfully created file: " + newFileName)
