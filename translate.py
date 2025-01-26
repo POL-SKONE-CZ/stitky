35674114e21403f986962bfe4dd20dc6b25bf07f
fadrny <48125221+fadrny@users.noreply.github.com>
2025-01-26 16:29:47 +0100

    revert changes

--- translate.py
+++ translate.py
@@ -10,7 +10,6 @@ for f in glob.glob('**/*.html', recursive=True):
         result = translator.translate_text(fr.read(), target_lang="SK", source_lang="CS", split_sentences="nonewlines", tag_handling="html")
     
     newFileName = f.replace(".html", ".sk.html")
-    
     with open(newFileName, "w", encoding='utf-8') as fw:
         fw.write(result.text)
     
